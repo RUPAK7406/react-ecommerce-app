ba87c5401fb17437753ce7a8808aec2e9783f512
RUPAK7406 <rupak.tangudu@gamil.com>
2021-05-25 07:53:12 +0000

    added files

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import Home from './components/Home/index'
+import LoginForm from './components/LoginForm/index'
+
+const App = () => <LoginForm />
+
+export default App
---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,74 @@
+// Write your JS code here
+import {Component} from 'react'
+import './index.css'
+
+class LoginForm extends Component {
+  renderUsernameField = () => {
+    const {username} = this.state
+
+    return (
+      <>
+        <label className="input-label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="username-input-field"
+          value={username}
+          onChange={this.onChangeUsername}
+          placeholder="Username"
+        />
+      </>
+    )
+  }
+
+  renderPasswordField = () => {
+    const {password} = this.state
+
+    return (
+      <>
+        <label className="input-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="password-input-field"
+          value={password}
+          onChange={this.onChangePassword}
+          placeholder="Password"
+        />
+      </>
+    )
+  }
+
+  render() {
+    return (
+      <div className="login-form-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          alt="website logo"
+          className="logo-website-mobile"
+        />
+
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+          className="login-image"
+          alt="website login"
+        />
+        <form className="form-container" onSubmit={this.submitForm}>
+          <div className="input-container">{this.renderUsernameField()}</div>
+          <div className="input-container">{this.renderPasswordField()}</div>
+          <div className="login-btn-container">
+            <button className="login-btn" type="button">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
